34f43a61eb3dca72dacf9f8ae9c50e66d9322737
shayanxmoradi <shayan.moradi92@gmail.com>
2025-05-09 18:36:19 +0200

    Initial commit

---
+++ src/main/java/org/example/springherojava24/SpringHeroJava24Application.java
@@ -0,0 +1,13 @@
+package org.example.springherojava24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHeroJava24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHeroJava24Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/springherojava24/SpringHeroJava24ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springherojava24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHeroJava24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
