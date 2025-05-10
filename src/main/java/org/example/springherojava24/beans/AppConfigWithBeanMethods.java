bc67cd87a6dccef4dc1b6d14a2a7b67c8ab8666c
shayanxmoradi <shayan.moradi92@gmail.com>
2025-05-10 09:20:38 +0200

    Custom Beans

---
+++ src/main/java/org/example/springherojava24/beans/AppConfigWithBeanMethods.java
@@ -0,0 +1,25 @@
+package org.example.springherojava24.beans;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class AppConfigWithBeanMethods {
+    @Bean(name = "appDataSource",initMethod = "init",destroyMethod = "close")
+    public SimpleDataSource myDataSource() {
+        System.out.println("x" +
+                           "x" +
+                           "x" +
+                           "AppConfig: Creating SimpleDataSource bean via @Bean method myDataSource().");
+        return new SimpleDataSource("jdbc:mydb://localhost:3306/mydatabase");
+    }
+
+    @Bean
+    public UserRepository userRepository() {
+        System.out.println("AppConfig: Creating UserRepository bean via @Bean method userRepository().");
+        SimpleDataSource ds = myDataSource();
+        return new JdbcUserRepositoryImpl(ds);
+    }
+}
