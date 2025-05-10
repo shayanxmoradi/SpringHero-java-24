55a8795d63d5809b12eddbe222519f818b333ff0
shayanxmoradi <shayan.moradi92@gmail.com>
2025-05-10 09:35:10 +0200

    Externalized Configuration

---
+++ src/main/java/org/example/springherojava24/Externalized_config/AppPropertiesConfig.java
@@ -0,0 +1,15 @@
+package org.example.springherojava24.Externalized_config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class AppPropertiesConfig {
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
---
+++ src/main/java/org/example/springherojava24/Externalized_config/ConfigDisplayService.java
@@ -0,0 +1,34 @@
+package org.example.springherojava24.Externalized_config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConfigDisplayService {
+    private final Environment environment;
+@Autowired
+    public ConfigDisplayService(Environment environment) {
+        this.environment = environment;
+    }
+    public void displayConfigs(){
+        System.out.println("hi" );
+        String appName = environment.getProperty("spring.application.name");
+        System.out.printf("app name: %s\n", appName)   ;
+        String appOwner = environment.getProperty("app.owner", "Default Owner Inc.");
+        System.out.println("Application Owner: " + appOwner);
+
+        // Get a property and convert its type
+        Boolean notificationsEnabled = environment.getProperty("feature.notifications.enabled", Boolean.class, true);
+        System.out.println("Notifications Enabled: " + notificationsEnabled);
+
+        // Get a property that might be missing
+        String nonExistentProp = environment.getProperty("some.missing.property");
+        System.out.println("Missing Property: " + nonExistentProp); // Will be null
+
+        // Check if a property exists
+        if (environment.containsProperty("db.url")) {
+            System.out.println("DB URL: " + environment.getProperty("db.url"));
+        }
+    }
+}
